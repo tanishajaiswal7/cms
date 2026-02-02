bd190398c5c331b4bba683454f84413f36c74d1e
Tanisha Jaiswal <tanishajaiswal757@gmail.com>
2026-02-02 14:22:43 +0530

    fix error

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://cmss-kvx9.onrender.com/",
+  baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
 });
 
